c6afbf4225947b9094da883c34b43e211d3f6670
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-09-13 08:40:21 +0100

    Update server.js
    changes for render

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,11 @@ const db = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 
 
